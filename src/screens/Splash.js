b0c0d03d9c959348c0dcbaf6a427b2d7d8a6b33e
Laurie Lay <llay@ippon.fr>
2022-12-20 11:48:54 -0500

    initial screens

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,10 +1,9 @@
-import Landing from './src/screens/Landing';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function Splash() {
   return (
     <View style={styles.container}>
-      <Landing />
+      
     </View>
   );
 }
@@ -17,3 +16,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default Splash;
