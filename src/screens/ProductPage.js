93ba38944cb9a5b882d776eabad166071ebf9fda
Laurie Lay <llay@ippon.fr>
2023-01-10 16:05:15 -0500

    add ProductPage horizontal imageScroll

--- src/screens/ProductPage.js
+++ src/screens/ProductPage.js
@@ -1,42 +1,70 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, FlatList } from 'react-native';
 import { globalStyles } from '../constants/GlobalStyles';
 import Button from '../components/Button';
 
 function ProductPage({route}) {
+  const [productImages, setProductImages] = useState([])
+
   const selectedProduct = route.params.product;
 
   function presshandler() {
     console.log('pressed');
   }
-  return (
+  
+  function renderImageItems(itemData) {
+    return (
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{uri:itemData.item}} />
+      </View>
+      )
+  }
+
+  function getProductImages() {
+      setProductImages(selectedProduct.images)
+  } 
+    
+  useEffect(()=>{
+    getProductImages();
+  })
+
+  return (<>
+  
     <View style={globalStyles.container}>
+      <FlatList 
+      horizontal={true}
+      data={productImages}
+      renderItem={renderImageItems}
+      />
       <ScrollView>
         <View style={styles.container}>
-          <Image style={styles.image} source={{uri:selectedProduct.images[1]}} />
           <Text style={styles.title}>{selectedProduct.title}</Text>
           <Text style={styles.subTitle}>{selectedProduct.description}</Text>
           <Text style={styles.price}>${selectedProduct.price}.00</Text>
         </View>
         <Button onPress={presshandler} text={'Add to Favorites'} backgroundColor={'blue'} color={'white'}/>
         <Button onPress={presshandler} text={'Purchase'} backgroundColor={'lightblue'} color={'black'}/>
-
       </ScrollView>
     </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 40,
     margin: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
   image: {
-    width: '80%',
+    width: 250,
     height: 250,
-    borderRadius: '50%'
+  },
+  imageContainer:{
+    flex: 1,
+    marginLeft: 15,
+    justifyContent: 'center',
   },
   title: {
     fontWeight: 'bold',
