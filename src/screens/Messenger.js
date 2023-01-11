d5042d481f62ce76c6c5bb687f05dedc52dcac4b
Laurie Lay <llay@ippon.fr>
2023-01-10 20:54:12 -0500

    update messenger styling

--- src/screens/Messenger.js
+++ src/screens/Messenger.js
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback, View, TextInput } from 'react-native';
 import { globalStyles } from '../constants/GlobalStyles';
+import Button from '../components/Button';
 
 function Messenger() {
   return (
     <View style={globalStyles.container}>
       <View style={styles.container}>
-      <Text>Messenger</Text>
+        <View style={styles.messageContainer}>
+        </View>
+          <KeyboardAvoidingView 
+            behavior="height"
+            style={styles.inputContainer}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+              <View style={styles.textInputContainer}>
+                <TextInput
+                style={styles.textInput}
+                autoCapitalize="none"
+                />
+                <Button text={'SEND'} color="white" onPress={()=>console.log('send')} backgroundColor={globalStyles.colors.primary100}/>
+              </View>
+            </TouchableWithoutFeedback> 
+        </KeyboardAvoidingView>
       </View>
     </View>
   );
@@ -14,10 +29,42 @@ function Messenger() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
+  messageContainer: {
+    flex: 3,
+    width: '100%',
+    marginHorizontal: 4,
+    borderRadius: 24,
+    backgroundColor: '#fff',
+    overflow: 'scroll',
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    width: '100%',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    justifyContent: 'center',
+  },
+  textInputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginHorizontal: 10,
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: globalStyles.colors.primary100,
+    borderRadius: 24,
+    fontSize: 16,
+    paddingHorizontal: 8,
+    marginVertical: 10,
+    width: '100%',
+    height: 42,
+  }
 });
 
 export default Messenger;
