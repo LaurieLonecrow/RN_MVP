7403c49217e05f8ca960319cfea2a44f6d2b9ab2
Laurie Lay <llay@ippon.fr>
2023-01-12 13:50:06 -0500

    add map component

---
+++ src/components/Map.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
+
+function Map() {
+  const [location, setLocation] = useState(null);
+
+    useEffect(() => {
+      (async () => {
+        let { status } = await requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+        let location = await getCurrentPositionAsync({});
+        setLocation({
+          lat: location.coords.latitude,
+          lng: location.coords.longitude
+        });
+      })();
+    }, [location]);
+
+    let locationPreview = <Text style={styles.waitingText}>Finding your current location...</Text>
+    if (location) {
+      const region = {
+        latitude: location.lat, 
+        longitude: location.lng,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      };
+      locationPreview = <MapView style={styles.map} initialRegion={region}></MapView>;
+    }
+    
+  return (
+    <View style={styles.container}>
+    {locationPreview}
+    </View>
+  )
+}
+
+export default Map;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  waitingText: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    flex: 1,
+  },
+});
--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -1,11 +1,16 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import ImagePicker from '../components/ImagePicker';
+import Map from '../components/Map';
 import { globalStyles } from '../constants/GlobalStyles';
 
+
 function Landing({navigation}) {
+  
+
   return (
     <View style={globalStyles.container}>
       <ImagePicker />
+      <Map />
     </View>
   );
 }
