ef4213dee89ef4f27713b8d1fc0afd2cabcbc21a
Laurie Lay <llay@ippon.fr>
2023-01-18 16:02:15 -0500

    add show user on maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,30 +1,57 @@
 import { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
 import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 import MapView, { Marker } from 'react-native-maps';
 import LoadingOverlay from './LoadingOverlay';
 import { getCurrentLocation } from '../util/helpers/getLocation';
 
+import { getUsers } from '../api/productsApi';
+
+
 function Map() {
+  const [ userlocations, setUserLocations ] = useState();
   const { location } = getCurrentLocation();
 
-    let locationPreview = <LoadingOverlay message={"Finding your current location..."}/>
-    if (location) {
+  async function getUserLocations() {
+    const userlocations = await getUsers();
+    const locationsArray = userlocations.map(each => ({"name": `${each.firstName} ${each.lastName}`, "location": each.location, "id": each.id}));
+    setUserLocations(locationsArray);
+  }
+
+  useEffect(() => {
+  getUserLocations()
+   },[]);
+
+    let locationPreview = <LoadingOverlay message={"Finding your people..."}/>
+    if (location && userlocations) {
       const region = {
         latitude: location.lat, 
         longitude: location.lng,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitudeDelta: 12,
+        longitudeDelta: 1,
       };
-      locationPreview = <MapView style={styles.map} initialRegion={region}>
+    locationPreview = 
+      <MapView style={styles.map} initialRegion={region}>
         <Marker
-          title="Picked Location"
+          title="You are Here"
           coordinate={{
             latitude: location.lat, 
             longitude: location.lng,
           }}
         />
-      </MapView>;
+          {userlocations.map((item) => {
+            return (
+               <Marker 
+                   key={item.id}
+                   coordinate={{
+                       latitude: item.location.latitude,
+                       longitude: item.location.longitude,
+                   }}
+                   title={item.name}
+              />
+            )
+        })}
+      </MapView>
     }
     
   return (
